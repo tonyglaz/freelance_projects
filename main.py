d4c1cb5ac6cf0274f49faa7105f133a9dc1c0816
tonyglaz <tonyglazunov@gmail.com>
2022-09-08 00:25:27 +0300

    https://www.lifetime.plus/ data parsing

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+import os
+import csv
+import json
+from datetime import datetime
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def collect_data():
+    t_date = datetime.now().strftime('%d_%m_%Y')
+    response = requests.get(url='https://www.lifetime.plus/api/analysis2')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open(f'info_{t_date}.json', 'w', encoding="utf-8") as file:
+        json.dump(response.json(), file, indent=4, ensure_ascii=False)  # все не-ASCII символы запишутся как есть
+
+    categories = response.json()['categories']
+    result = []
+    # trans_table = str.maketrans({'γ': 'y', 'β': 'B'})
+    for c in categories:
+        c_name = c.get('name').strip()
+        c_items = c.get('items')
+
+        for item in c_items:
+            item_name = item.get('name').strip()
+            item_price = item.get('price')
+            item_desc = item.get('description').strip()  # translate(trans_table).
+            item_wt = item.get('days')
+            item_bio = item.get('biomaterial')
+            result.append([c_name, item_name, item_bio, item_desc, item_price, item_wt])
+    with open(f'result_{t_date}.csv', 'a', encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                'Категория',
+                'Анализ',
+                'Биоматериал',
+                'Описание',
+                'Стоимость',
+                'Готовность дней'
+            )
+        )
+        writer.writerows(
+            result
+        )
+#TODO оптимизировать
+
+def main():
+    collect_data()
+
+
+if __name__ == "__main__":
+    main()
