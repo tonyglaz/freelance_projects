28aa939afbedc8f4e2b4f46f43866272377f7cc9
tonyglaz <tonyglazunov@gmail.com>
2022-09-19 15:03:42 +0300

    problem fixed

--- mvideo_parsing/main_pagination.py
+++ mvideo_parsing/main_pagination.py
@@ -3,6 +3,7 @@ import requests
 import json
 from config import cookies, headers
 import math
+from time import sleep
 
 
 def get_data():
@@ -10,26 +11,25 @@ def get_data():
         'categoryId': '118',
         'offset': '0',
         'limit': '24',
+        'filterParams': 'WyJ0b2xrby12LW5hbGljaGlpIiwiIiwiZGEiXQ==',
         'doTranslit': 'true',
     }
 
     if not os.path.exists('data'):
         os.mkdir('data')
     s = requests.Session()
-
     response = s.get('https://www.mvideo.ru/bff/products/listing', params=params, cookies=cookies,
-                     headers=headers).json()
-
+                            headers=headers).json()
     total_items = response.get('body').get('total')
     if total_items is None:
         return '[!] no items [!]'
     page_count = math.ceil(total_items / 24)
-    # print(f'total positions: {page_count} total pages: {total_items}')
-
+    print(f'total positions: {total_items} total pages: {page_count}')
     products_ids = {}
     products_description = {}
     products_prices = {}
     for i in range(page_count):
+        #sleep(3)
         offset = f'{i * 24}'
         params = {
             'categoryId': '118',
@@ -37,9 +37,9 @@ def get_data():
             'limit': '24',
             'doTranslit': 'true',
         }
-        response = s.get('https://www.mvideo.ru/bff/products/listing', params=params, cookies=cookies,
-                         headers=headers).json()
+        response = s.get('https://www.mvideo.ru/bff/products/listing', params=params, cookies=cookies,headers=headers).json()
         products_ids_list = response.get('body').get('products')
+        #print(products_ids_list)
         products_ids[i] = products_ids_list
         json_data = {
             'productIds': products_ids_list,
@@ -57,38 +57,40 @@ def get_data():
             },
             'multioffer': False,
         }
-        response = s.post('https://www.mvideo.ru/bff/product-details/list', cookies=cookies, headers=headers,
-                          json=json_data).json()
+        response = s.post('https://www.mvideo.ru/bff/product-details/list', cookies=cookies, headers=headers,json=json_data).json()
+        print(response['body'])
         products_description[i] = response
-       # print(response)
+        with open(f'data/2_req/{i+1}_prod_desc.json', 'w', encoding='utf-8') as file:
+            json.dump(products_description[i], file, indent=4, ensure_ascii=False)
+        # print(response)
         products_ids_str = ','.join(products_ids_list)
 
-        # params = {
-        #     'productIds': products_ids_str,
-        #     'addBonusRubles': 'true',
-        #     'isPromoApplied': 'true',
-        # }
-        # response = s.get('https://www.mvideo.ru/bff/products/prices', params=params, cookies=cookies,
-        #                  headers=headers).json()
-        # material_prices = response.get('body').get('materialPrices')
-        #
-        # for item in material_prices:
-        #     item_id = item.get('price').get('productId')
-        #     item_base_price = item.get('price').get('basePrice')
-        #     item_sale_price = item.get('price').get('salePrice')
-        #     item_bonus = item.get('bonusRubles').get('total')
-        #     products_prices[item_id] = {
-        #         'item_basePrice': item_base_price,
-        #         'item_salePrice': item_sale_price,
-        #         'item_bonus': item_bonus
-        #     }
+        params = {
+            'productIds': products_ids_str,
+            'addBonusRubles': 'true',
+            'isPromoApplied': 'true',
+        }
+        response = s.get('https://www.mvideo.ru/bff/products/prices', params=params, cookies=cookies,
+                         headers=headers).json()
+        material_prices = response.get('body').get('materialPrices')
+
+        for item in material_prices:
+            item_id = item.get('price').get('productId')
+            item_base_price = item.get('price').get('basePrice')
+            item_sale_price = item.get('price').get('salePrice')
+            item_bonus = item.get('bonusRubles').get('total')
+            products_prices[item_id] = {
+                'item_basePrice': item_base_price,
+                'item_salePrice': item_sale_price,
+                'item_bonus': item_bonus
+            }
         print(f'[+] Finished {i + 1} of the {page_count} pages')
     with open('data/1_product_ids.json', 'w') as file:
         json.dump(products_ids, file, indent=4, ensure_ascii=False)
-    with open('data/2_product_description.json', 'w') as file:
+    with open('data/2_product_description.json', 'w',encoding='utf-8') as file:
         json.dump(products_description, file, indent=4, ensure_ascii=False)
-    # with open('data/3_products_prices.json', 'w') as file:
-    #     json.dump(products_prices, file, indent=4, ensure_ascii=False)
+    with open('data/3_products_prices.json', 'w') as file:
+        json.dump(products_prices, file, indent=4, ensure_ascii=False)
 
 
 def get_result():
